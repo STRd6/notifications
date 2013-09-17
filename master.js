9532b98a05da20f0fc84650ed65ab60cbb2f48b3
Daniel X Moore <yahivin@gmail.com>
2013-09-17 16:18:43 -0700

    Built master in browser in strd6.github.io/editor

---
+++ master.js
@@ -0,0 +1,35 @@
+(function() {
+  var Notifications;
+
+  Notifications = function() {
+    var errors, notices;
+    notices = Observable([]);
+    errors = Observable([]);
+    return {
+      classicError: function(request, error, message) {
+        notices([]);
+        if (request.responseJSON) {
+          message = JSON.stringify(request.responseJSON, null, 2);
+        } else {
+          if (message == null) {
+            message = request;
+          }
+        }
+        return errors([message]);
+      },
+      notify: function(message) {
+        notices([message]);
+        return errors([]);
+      },
+      push: function(message) {
+        return notices.push(message);
+      },
+      errors: erros,
+      notices: notices,
+      template: require('./template')
+    };
+  };
+
+  module.exports = Notifications;
+
+}).call(this);
